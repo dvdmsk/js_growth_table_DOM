9dd5e2bdf3b2f1e708551a3e6402177523f36b03
David Moskalenko <dvdmsk21@gmail.com>
2025-02-20 16:36:26 +0200

    fix code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,102 +2,83 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const table = document.querySelector('.field');
+  const tbody = table.querySelector('tbody');
 
-  checkTable(table);
+  checkTable();
 
   document.addEventListener('click', function (e) {
     const btn = e.target.closest('.button');
 
-    if (btn === null) {
-      return;
-    }
-
-    const rows = table.querySelectorAll('tr');
-
-    if (rows.length === 0) {
+    if (!btn) {
       return;
     }
 
     switch (btn.classList[0]) {
       case 'append-row':
-        addRow(table, rows);
+        addRow();
         break;
       case 'remove-row':
-        removeRow(rows);
+        removeRow();
         break;
       case 'append-column':
-        addColumn(rows);
+        addColumn();
         break;
       case 'remove-column':
-        removeColumn(rows);
+        removeColumn();
         break;
     }
-    checkTable(table);
+    checkTable();
   });
 
-  function addColumn(rows) {
-    const rowsArr = [...rows];
+  function addColumn() {
+    const rows = table.querySelectorAll('tr');
 
-    rowsArr.forEach((el) => {
+    rows.forEach((row) => {
       const newColumn = document.createElement('td');
 
-      el.append(newColumn);
+      row.appendChild(newColumn);
     });
   }
 
-  function removeColumn(rows) {
-    const rowsArr = [...rows];
+  function removeColumn() {
+    const rows = table.querySelectorAll('tr');
 
-    rowsArr.forEach((el) => {
-      if (el.cells.length > 0) {
-        el.cells[el.cells.length - 1].remove();
+    rows.forEach((row) => {
+      if (row.cells.length > 0) {
+        row.deleteCell(-1);
       }
     });
   }
 
-  function addRow(tableTarget, rows) {
-    const clonedRow = rows[0].cloneNode(true);
+  function addRow() {
+    const firstRow = table.querySelector('tr');
 
-    tableTarget.querySelector('tbody').append(clonedRow);
-  }
+    if (firstRow) {
+      const newRow = firstRow.cloneNode(true);
 
-  function removeRow(rows) {
-    if (rows.length > 0) {
-      rows[rows.length - 1].remove();
+      tbody.appendChild(newRow);
     }
   }
 
-  function checkTable(tableTarget) {
-    const rows = tableTarget.querySelectorAll('tr').length;
-    const cols =
-      rows > 0 ? tableTarget.querySelectorAll('tr')[0].cells.length : 0;
+  function removeRow() {
+    const rows = table.querySelectorAll('tr');
+
+    if (rows.length > 0) {
+      tbody.removeChild(rows[rows.length - 1]);
+    }
+  }
+
+  function checkTable() {
+    const rows = table.querySelectorAll('tr').length;
+    const cols = rows > 0 ? table.querySelector('tr').cells.length : 0;
     const btnAddCol = document.querySelector('.append-column');
     const btnRemCol = document.querySelector('.remove-column');
     const btnRemRow = document.querySelector('.remove-row');
     const btnAddRow = document.querySelector('.append-row');
 
-    if (rows >= 10) {
-      btnAddRow.disabled = true;
-    } else {
-      btnAddRow.disabled = false;
-    }
-
-    if (rows <= 2) {
-      btnRemRow.disabled = true;
-    } else {
-      btnRemRow.disabled = false;
-    }
-
-    if (cols >= 10) {
-      btnAddCol.disabled = true;
-    } else {
-      btnAddCol.disabled = false;
-    }
-
-    if (cols <= 2) {
-      btnRemCol.disabled = true;
-    } else {
-      btnRemCol.disabled = false;
-    }
+    btnAddRow.disabled = rows >= 10;
+    btnRemRow.disabled = rows <= 2;
+    btnAddCol.disabled = cols >= 10;
+    btnRemCol.disabled = cols <= 2;
   }
 });
